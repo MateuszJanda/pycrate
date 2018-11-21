781eb98d63ef815d9be97dc8746234dcf625e4c5
Mateusz Janda <mateusz.janda@gmail.com>
2018-11-21 17:05:50 +0100

    Added example.py file with decoding not default payload

---
+++ example.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Compile Python sources from your own ASN.1 definitions
+python ./tools/pycrate_asn1compile.py -i ./pycrate_asn1dir/3GPP_EUTRAN_SUBSET/ -o ltemini -j
+"""
+
+import ltemini
+from binascii import unhexlify, hexlify
+
+
+def decode_own_payload():
+    # List all (one) definitions
+    # ['PCCH-Message-Definition'] - object will have all '-' replaced with '_'
+    # print(list(ltemini.GLOBAL.MOD.keys()))
+
+    # List all PDUs from definitions
+    # print(dir(ltemini.PCCH_Message_Definition))
+
+    pdu = ltemini.PCCH_Message_Definition.PCCH_Message
+    pdu.from_uper(unhexlify('0b00'))
+    print(pdu.to_asn1())
+
+
+if __name__ == '__main__':
+    decode_own_payload()
